6e6b73dc4d16ab58777d8d977acb0669cfe644ce
yeblo <yeblo95@yahoo.com>
2019-11-12 16:36:08 -0600

    Add files via upload

---
+++ Delivery/js/scroll.js
@@ -0,0 +1,20 @@
+var $window = $(window), 
+    win_height_padded = $window.height() * 0.1;
+
+$window.on('scroll', revealOnScroll);
+
+function revealOnScroll(){
+    var scrolled = $window.scrollTop();
+    $(".revealOnScroll:not(.animated)").each(function () {
+        var $this     = $(this),  offsetTop = $this.offset().top;
+         if (scrolled + win_height_padded > offsetTop) {
+             if ($this.data('timeout')) {
+                  window.setTimeout(function(){
+                      $this.addClass('animated ' + $this.data('animation'));
+                      }, parseInt($this.data('timeout'),10));
+                 } else {
+          $this.addClass('animated ' + $this.data('animation'));
+        }
+      }
+    });
+}
